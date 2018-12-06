a11fc02a9996dfbfd7890d4bfd6ea3c1449e63bd
George Gordon <ggord@upenn.edu>
2018-12-06 15:55:35 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+# git 'https://github.com/demery/xlsx_to_pqc.git', branch: 'development' do
+  gem 'xlsx_to_pqc_xml', git: 'https://github.com/demery/xlsx_to_pqc.git', branch: 'development'
+# end
+
+gem 'yard'
+
+gem 'pry'
---
+++ yaml_test_script.rb
@@ -0,0 +1,26 @@
+require 'xlsx_to_pqc_xml'
+require 'yaml'
+require 'pry'
+require 'pp'
+require 'json'
+
+require './script'
+
+PE = PlaybillExtractor.new('/Users/ggord/Desktop/Playbills_9970657833503681.xlsx')
+
+pp PE.extract_event
+=begin
+  config = YAML.load open('data/performance.yml').read
+  config[:sheet_position] = 2
+  xlsx_data = XlsxToPqcXml::XlsxData.new xlsx_path: '/Users/ggord/Desktop/Playbills_9970657833503681.xlsx', config: config
+  pp xlsx_data.data
+
+  # puts xlsx_data.errors # => {} empty errors hash
+
+  p ARGV.first
+
+
+  contributors = xlsx_data.data[1..-1]
+
+  puts xlsx_data.data.first.merge({contributors: contributors}).to_json
+=end
