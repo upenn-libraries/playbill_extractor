eeeb2cf71115f0e4dde089ca44ed5ae4d1283992
George Murnaghan Gordon <gordonmgeorge@gmail.com>
2018-12-21 12:47:56 -0500

    rework to run on folder of xlsx files
    rework to run on folder of xlsx files

--- script.rb
+++ script.rb
@@ -1,12 +1,12 @@
-require 'json'
-require './extractor'
+# require 'json'
+# require './extractor'
 
-path = ARGV.first
+# path = ARGV.first
 
-abort 'No filepath given'        unless path
-abort "No file found at #{path}" unless File.exist?(path)
+# abort 'No filepath given'        unless path
+# abort "No file found at #{path}" unless File.exist?(path)
 
-puts JSON.pretty_generate(PlaybillExtractor.new(path).output_data)
+# puts JSON.pretty_generate(PlaybillExtractor.new(path).output_data)
 
 
 
@@ -21,16 +21,18 @@ abort "No folder found at #{folder_path}" unless Dir.exist?(folder_path)
 
 xlsx_files  = Dir.entries(folder_path).select{ |e| e =~ /^[^\.~].*\.xlsx$/ }
 
-xlsx_files.each do |file|
-  puts "working on #{file}"
+total = xlsx_files.size 
+
+xlsx_files.each_with_index do |file, i|
+  print "extracting from #{file} (#{i + 1}/#{total})..."
   file_path = folder_path + file
   p_e = PlaybillExtractor.new(file_path)
 
-
   File.open "#{file_path[0..-6]}.json", 'w+' do |f|
-    f.puts JSON.pretty_generate(p_e.result.to_json)
+    f.puts p_e.get_result.to_json
   end
-
+  # percent = ((i + 1).to_f / total * 100).round
+  puts "	saved JSON"# (#{percent}% done)"
 end
 
 
